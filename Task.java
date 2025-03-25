fc91b9507eeaf2322f7225bdc8e1e45e31531729
ElinaAIT <elinabaktygulovaaa28@gmail.com>
2025-03-25 21:30:20 +0600

    Update Task.java

--- Task.java
+++ Task.java
@@ -1,97 +1,43 @@
-import java.time.LocalDateTime;
-import java.util.*;
+public class Task implements Comparable<Task>{
+    private int priority;
+    private String name;
+    private int duration;
 
-public class TaskScheduler{
-    private PriorityQueue<Task> scheduledTasks;
-    private ArrayDeque<Task> pendingTasks;
-    private HashMap<Task,ArrayList<LocalDateTime>> completedTasks;
-
-    public TaskScheduler(){
-        // Comparator method
-        /*scheduledTasks = new PriorityQueue<>((a,b)->{
-            if(a.getPriority()!=b.getPriority())
-             return Integer.compare(b.getPriority(),a.getPriority());
-            else{
-                return Integer.compare(a.getDuration(),b.getDuration());
-            }
-        });*/
-        scheduledTasks = new PriorityQueue<>();
-        pendingTasks = new ArrayDeque<>();
-        completedTasks = new HashMap<>();
+    public Task(int priority, String name, int duration) {
+        this.priority = priority;
+        this.name = name;
+        this.duration = duration;
     }
 
-    public void addTask(Task t){
-        scheduledTasks.offer(t);
-    }
-
-    public void printAllTasks(){
-        System.out.println("Scheduled Tasks (sorted by priority):");
-        for(Task t: scheduledTasks){
-            System.out.println("["+t.getPriority()+"] "+
-                    t.getName() + " ( "+t.getDuration()+")");
-        }
-
-        System.out.println("Pending Tasks(FIFO order):");
-        for(Task t:pendingTasks){
-            System.out.println(t.getName() + ", " + t.getPriority()+", "+t.getDuration());
-        }
-
-        System.out.println("Completed Tasks: ");
-        for(Map.Entry<Task,ArrayList<LocalDateTime>> entry:completedTasks.entrySet()){
-            System.out.println(entry.getKey().getName() + ": (" +
-                    entry.getValue().get(0)+","+
-                    entry.getValue().get(1));
+    public int compareTo(Task t){
+        if(this.getPriority()!=t.getPriority())
+            return Integer.compare(t.getPriority(),this.getPriority());
+        else{
+            return Integer.compare(this.getDuration(),t.getDuration());
         }
     }
-
-    public void delayTask(Task t){
-        pendingTasks.offer(t);
-        scheduledTasks.remove(t);
+    public int getPriority() {
+        return priority;
     }
 
-    public void processTask(){
-        if(!scheduledTasks.isEmpty()){
-            Task t = scheduledTasks.poll();
-            System.out.println("Processing Task: "+t.getPriority()+", "+
-                    t.getName()+", "+t.getDuration());
-            LocalDateTime timenow = LocalDateTime.now();
-            ArrayList<LocalDateTime> list = new ArrayList<>();
-            list.add(timenow);
-            list.add(timenow.plusMinutes(t.getDuration()));
-            completedTasks.put(t,list);
-            //completedTasks.put(t,new ArrayList<>(Arrays.asList(timenow,timenow.plusMinutes(t.getDuration()))))
-            return;
-        }
-        if(!pendingTasks.isEmpty()){
-            Task t = pendingTasks.poll();
-            System.out.println("Processing Task: "+t.getPriority()+", "+
-                    t.getName()+", "+t.getDuration());
-            return;
-        }
-        System.out.println("No more tasks left to be executed. Quitting.");
-        return;
+    public void setPriority(int priority) {
+        this.priority = priority;
     }
-}
 
-class TaskSchedulerMain{
-    public static void main(String[] args) {
-        TaskScheduler scheduler = new TaskScheduler();
-        Task t = new Task(3,"Code Review", 20);
-        scheduler.addTask(t);
-        scheduler.addTask(new Task(5,"System Update", 45));
-        Task t1 = new Task(2,"Database Backup", 30);
-        scheduler.addTask(t1);
-        scheduler.addTask(new Task(5,"Deploy New Feature", 50));
-        scheduler.addTask(new Task(4,"Bug Fixing", 25));
+    public String getName() {
+        return name;
+    }
 
-        scheduler.printAllTasks();
-        scheduler.processTask();
-        scheduler.delayTask(t);
-        scheduler.printAllTasks();
-        scheduler.delayTask(t1);
-        scheduler.printAllTasks();
-        scheduler.processTask();
-        scheduler.printAllTasks();
-        scheduler.processTask();
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    
+    public void setDuration(int duration) {
+        this.duration = duration;
     }
 }
